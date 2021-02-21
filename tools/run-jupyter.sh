74e27e8808fd212c18f032f904e707c6a740c938
Bogdan Dobrica <bdobrica@gmail.com>
2021-02-21 17:27:14 +0200

    Update run-jupyter.sh
    - changing the directory to be sure that we're in the right place when we start Jupyter notebook;

--- tools/run-jupyter.sh
+++ tools/run-jupyter.sh
@@ -2,6 +2,8 @@
 INTERNAL_PORT=8888 ## This is the port on which Jupyter Notebook runs on Raspberry Pi
 EXTERNAL_PORT=8889 ## This is the port on which Jupyter Notebook is accessible from outside
 
+## In order to run the Jupyer notebook at startup, first we must be sure that we're in the correct directory
+cd /home/pi
 ## Run the Jupyter notebook by specifying that there's no local browser (--no-browser),
 ##  the port on which to run (--port) and to allow remote access (--NotebookApp.allow_remote_access=1).
 jupyter notebook --no-browser --port=${INTERNAL_PORT} --NotebookApp.allow_remote_access=1 &
