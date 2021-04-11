eb8f2f9233dce5e1614b9658e8f154d2f18b2e25
Bogdan Dobrica <bdobrica@gmail.com>
2021-04-11 21:07:53 +0300

    Update setup.sh
    - create the venv and install the necessary python modules;

--- apps/security-cam/setup.sh
+++ apps/security-cam/setup.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+if [ ! -d ~/.venvs/python-security-cam ]; then
+	python3 -m venv ~/.venvs/python-security-cam
+fi
+
+source ~/.venvs/python-security-cam/bin/activate
+
+pip3 install opencv-contrib-python==4.1.0.25
+pip3 install flask
+pip3 install imutils
+pip3 install picamera
+
+deactivate
