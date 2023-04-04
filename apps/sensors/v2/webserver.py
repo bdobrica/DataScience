73e336e6e52a15bc9688ad2c8870568115cce49d
Bogdan Dobrica <bdobrica@gmail.com>
2023-04-04 21:48:36 +0300

    Cleaning up V2
    - added logger;
    - and how to use constants;

--- apps/sensors/v2/webserver.py
+++ apps/sensors/v2/webserver.py
@@ -10,6 +10,7 @@ import plotly.express as px
 from flask import Flask, render_template, request
 
 SCRIPT_NAME = Path(__file__).stem
+DEFAULT_FEATURE = "temperature_1_value"
 logger = logging.getLogger(SCRIPT_NAME)
 app = Flask(SCRIPT_NAME)
 
@@ -17,8 +18,8 @@ app = Flask(SCRIPT_NAME)
 def parse_args(args: list = None) -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         prog=SCRIPT_NAME,
-        description="Read data from Arduino",
-        epilog=f"{SCRIPT_NAME} --batch-length 12 --output-path /home/pi/weather",
+        description="Display sensor data using Plotly with interactive controls",
+        epilog=f"{SCRIPT_NAME} --input-path /home/pi/weather",
     )
 
     def validate_path(path: str) -> Path:
@@ -32,26 +33,28 @@ def parse_args(args: list = None) -> argparse.Namespace:
         "--input-path",
         type=validate_path,
         default=Path("/home/pi/weather"),
-        help="Path to write output files",
+        help="Path to read the data files from",
     )
     args = parser.parse_args(args)
     return args
 
 
-def get_data(selected: list) -> pd.DataFrame:
-    df = pd.concat([pd.read_csv(csv_path) for csv_path in app.config["input_path"].glob("*.csv")])
+def get_data() -> pd.DataFrame:
+    csv_files = app.config["input_path"].glob("*.csv")
+    df = pd.concat([pd.read_csv(csv_path) for csv_path in csv_files])
     df.sort_values(by="datetime", inplace=True)
+    logger.info(f"Read {len(df)} rows from {len(csv_files)} files")
     return df
 
 
 @app.route("/")
 def index():
-    selected = ["temperature_1_value"]
+    selected = [DEFAULT_FEATURE]
     df = get_data(selected)
     fig = px.line(df, x="datetime", y=selected)
     graph = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return render_template(
-        "index-dynamic.html",
+        "index.html",
         graph=graph,
         parameters={col: col in selected for col in df.columns[1:]},
     )
@@ -60,8 +63,7 @@ def index():
 @app.route("/data")
 def data():
     selected = request.args.getlist("features[]")
-    print(selected)
-    df = get_data(selected)
+    df = get_data()
     fig = px.line(df, x="datetime", y=selected)
     graph = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graph
@@ -70,4 +72,5 @@ def data():
 if __name__ == "__main__":
     args = parse_args()
     app.config["input_path"] = args.input_path
+    logger.info(f"Starting {SCRIPT_NAME} with input path {args.input_path}")
     app.run(host="0.0.0.0")
