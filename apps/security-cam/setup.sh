6d7d1e7a5c5f952efb9b2c3edfe35dc6c29eec6f
Bogdan Dobrica <bdobrica@gmail.com>
2023-06-05 13:33:13 +0300

    Update setup.sh

--- apps/security-cam/setup.sh
+++ apps/security-cam/setup.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+SECURITY_CAM_VENV="python-security-cam"
 
-if [ ! -d ~/.venvs/python-security-cam ]; then
-	python3 -m venv ~/.venvs/python-security-cam
+if [ ! -d ~/.venvs/${SECURITY_CAM_VENV} ]; then
+	python3 -m venv ~/.venvs/${SECURITY_CAM_VENV}
+
+    source ~/.venvs/${SECURITY_CAM_VENV}/bin/activate
+
+    pip3 install opencv-contrib-python-headless
+    pip3 install flask
+
+    deactivate
+
+
+    ## Copy the missing packages from the system to the virtual environment
+    VENV_SITE_PACKAGES=$(find ~/.venvs/${SECURITY_CAM_VENV} -name "site-packages" | head -n 1)
+    NEEDED_PACKAGES=( "pykms" "simplejpeg" "pidng" "piexif" "prctl" "v4l2" "libcamera" "picamera2" )
+    for NEEDED_PACKAGE in "${NEEDED_PACKAGES[@]}"; do
+        find /usr/lib/python3/dist-packages -maxdepth 1 -name "*${NEEDED_PACKAGE}*" | while read d; do
+            cp -r "$d" "${VENV_SITE_PACKAGES}/$(basename "$d")";
+        done
+    done
 fi
-
-source ~/.venvs/python-security-cam/bin/activate
-
-pip3 install opencv-contrib-python-headless
-pip3 install flask
-pip3 install imutils
-pip3 install picamera
-
-deactivate
